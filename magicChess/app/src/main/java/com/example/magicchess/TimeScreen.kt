dc729720ba57b64816cc1a951c691de3a93fd6c2
Виноградова Елизавета <li_wi16@mail.ru>
2025-03-16 22:16:37 +0300

    time.kt

---
+++ magicChess/app/src/main/java/com/example/magicchess/TimeScreen.kt
@@ -0,0 +1,31 @@
+package com.example.magicchess
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TimeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_time_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button_back_time: Button = findViewById(R.id.backTime)
+
+        button_back_time.setOnClickListener {
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
