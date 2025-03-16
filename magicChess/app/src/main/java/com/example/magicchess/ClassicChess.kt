2b284eddf5560a12230ffd18ef23923f85195b1d
Виноградова Елизавета <li_wi16@mail.ru>
2025-03-16 22:13:05 +0300

    classic.kt

---
+++ magicChess/app/src/main/java/com/example/magicchess/ClassicChess.kt
@@ -0,0 +1,29 @@
+package com.example.magicchess
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ClassicChess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_classic_chess)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button_back_crazy: Button = findViewById(R.id.backClassicChess)
+
+        button_back_crazy.setOnClickListener {
+            val intent = Intent(this, ModeScreen::class.java)
+            startActivity(intent)
+        }
+    }
+}
