5512d457c372d11b5331925bae3284d1dd0494cf
Виноградова Елизавета <li_wi16@mail.ru>
2025-03-16 22:14:00 +0300

    crazy.kt

---
+++ magicChess/app/src/main/java/com/example/magicchess/CrazyChess.kt
@@ -0,0 +1,30 @@
+package com.example.magicchess
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CrazyChess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_crazy_chess)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button_back_crazy: Button = findViewById(R.id.backCrazyChess)
+
+        button_back_crazy.setOnClickListener {
+            val intent = Intent(this, ModeScreen::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
