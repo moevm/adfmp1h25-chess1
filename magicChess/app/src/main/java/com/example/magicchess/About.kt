6ba6fca65a5e2c93682c8653fe0d7d12d4ccdbdd
Виноградова Елизавета <li_wi16@mail.ru>
2025-03-18 21:20:00 +0300

    add changes

--- magicChess/app/src/main/java/com/example/magicchess/About.kt
+++ magicChess/app/src/main/java/com/example/magicchess/About.kt
@@ -1,28 +1,20 @@
 package com.example.magicchess
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ClassicChess : AppCompatActivity() {
+class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_classic_chess)
+        setContentView(R.layout.activity_about)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about_2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button_back_crazy: Button = findViewById(R.id.backClassicChess)
-
-        button_back_crazy.setOnClickListener {
-            finish()
         }
     }
 }
--- magicChess/app/src/main/java/com/example/magicchess/ColorScreen.kt
+++ magicChess/app/src/main/java/com/example/magicchess/ColorScreen.kt
@@ -13,21 +13,37 @@ class ColorScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_color_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about_2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
 
         val button_white_color: Button = findViewById(R.id.white)
         val button_black_color: Button = findViewById(R.id.black)
         val button_random_color: Button = findViewById(R.id.random)
         val button_back_color: Button = findViewById(R.id.backColor)
 
-        button_back_color.setOnClickListener {
-            val intent = Intent(this, MainScreen::class.java)
+        button_white_color.setOnClickListener {
+            val intent = Intent(this, ModeScreen::class.java)
+            intent.putExtra("color", "white")
             startActivity(intent)
+        }
+
+        button_black_color.setOnClickListener {
+            val intent = Intent(this, ModeScreen::class.java)
+            intent.putExtra("color", "black")
+            startActivity(intent)
+        }
+
+        button_random_color.setOnClickListener {
+            val intent = Intent(this, ModeScreen::class.java)
+            intent.putExtra("color", "random")
+            startActivity(intent)
+        }
+
+        button_back_color.setOnClickListener {
+            finish()
         }
     }
 }
