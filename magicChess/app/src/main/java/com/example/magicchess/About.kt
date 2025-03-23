d7caa206192793edf09cf7b71bda5c6c9957924b
TokGrin <90797638+TokGrin@users.noreply.github.com>
2025-03-23 23:19:49 +0300

    update about ui

--- magicChess/app/src/main/java/com/example/magicchess/About.kt
+++ magicChess/app/src/main/java/com/example/magicchess/About.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import kotlin.random.Random
+
 
 class About : AppCompatActivity() {
+    private lateinit var backButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_about)
+        backButton = findViewById(R.id.buttonBack)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about_2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
